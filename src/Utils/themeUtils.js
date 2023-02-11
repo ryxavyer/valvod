72c8166b48f363bd3dd8e917d5016d732a3d3373
Ryan Fernandez <ryxavyer@gmail.com>
2023-02-10 20:57:56 -0600

    added flamingo theme

--- src/Utils/themeUtils.js
+++ src/Utils/themeUtils.js
@@ -8,6 +8,7 @@ const DARK_THEMES = {
     "coffee": "dark",
     "dusk": "dark",
     "midnight": "dark",
+    "flamingo": "dark",
 }
 
 export const ALL_THEMES = {...LIGHT_THEMES, ...DARK_THEMES}
@@ -46,6 +47,41 @@ const getTheme = (theme) => {
             text: {
                 primary: grey[900],
                 secondary: grey[800],
+            },
+        }
+    }
+    if (theme === "flamingo") {
+        return {
+            // palette values for flamingo mode
+            primary: {
+                main: '#d4778e',
+                500: '#d4778e',
+            },
+            secondary: {
+                main: '#7d364d',
+            },
+            unread: {
+                main: '#FF0000',
+            },
+            warning: {
+                main: '#fdd835'
+            },
+            online:{
+                main: '#31EC69'
+            },
+            working: {
+                main: "#B68F61"
+            },
+            offline: {
+                main: "#0C0C0C"
+            },
+            background: {
+                default: '#542434',
+                paper: '#542434',
+            },
+            text: {
+                primary: '#fff',
+                secondary: grey[400],
             },
         }
     }
